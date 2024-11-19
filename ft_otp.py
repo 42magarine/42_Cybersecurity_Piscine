a8837ebfaf15547ec1ec923d03f1973ca8ab9adb
maott <maott@student.42berlin.de>
2024-11-19 16:36:08 +0100

    validate key file

--- ft_otp.py
+++ ft_otp.py
@@ -13,6 +13,8 @@
 # **************************************************************************** #
 
 import argparse
+import os
+import re   # regular expression
 
 RESET = "\033[0m"
 RED = "\033[31m"
@@ -25,23 +27,59 @@ def parse_arguments():
 
     # Create a mutually exclusive group
     group = parser.add_mutually_exclusive_group(required=True)
-
-    # Define the command-line arguments
     group.add_argument('-g', action='store_true', help='ggg')
     group.add_argument('-k', action='store_true', help='kkk')
+
+    # Add a positional argument for the file
     parser.add_argument('file', help='file')
 
-    # Parse the command-line arguments
     args = parser.parse_args()
+
+    # Validate that the file exists
+    if not os.path.isfile(args.file):
+        print(f"{RED}error: '{args.file}' is not a valid file path or does not exist.{RESET}")
+        exit(1)
+
+    # Validate that the file is readable
+    if not os.access(args.file, os.R_OK):
+        print(f"{RED}error: '{args.file}' is not readable. Check permissions.{RESET}")
+        exit(1)
 
     return args
 
+def generate_key(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            key = file.read()
+
+    except Exception as e:
+        print(f"{RED}error: Failed to read file: {e}{RESET}")
+        return
+
+    # Strip leading/trailing whitespace, including newlines
+    key = key.strip()
+
+    # Check for minimum length
+    if len(key) < 64: # oder != 64
+        print(f"{RED}error: key must be at least 64 hexadecimal characters.{RESET}")
+        return
+
+    # Validate against the pattern
+    if not re.fullmatch(r'[0-9a-fA-F]+', key):
+        print(f"{RED}error: key must only contains hexadecimal characters.{RESET}")
+        return
+
+    print(f"{GREEN}ok{RESET}")
+
 def main():
     args = parse_arguments()
+
     if args.g:
-        print(f"-g: {args.g}")
-    if args.k:
+        generate_key(args.file)
+    elif args.k:
         print(f"-k: {args.k}")
+
+    exit(0)
 
 if __name__ == "__main__":
     main()
